c7f663712b5d210f2c8142875215a2840c91949b
Gustice <jakob.ger@gmail.com>
2021-02-18 22:52:52 +0100

    tests: Unit-Tests for Web-Project almost working

--- Firmware/unitTests/MyWeb/TestMain.cpp
+++ Firmware/unitTests/MyWeb/TestMain.cpp
@@ -1,6 +1,11 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "cJSON.h"
+#include "esp_log.h"
+#include "esp_err.h"
+#include "esp_http_server.h"
+#include "freertos/FreeRTOS.h"
+
 
 static bool stringIsEqual(const char * expected, const char * actual);
 
@@ -23,3 +28,30 @@ static bool stringIsEqual(const char * expected, const char * actual)
     UNSCOPED_INFO("Actual " << actual);
     return false;
 }
+
+
+
+
+
+
+
+void esp_log_write(esp_log_level_t level, const char* tag, const char* format, ...)
+{
+    
+}
+
+esp_err_t xQueueSend( QueueHandle_t xQueue, const void * const pvItemToQueue, TickType_t xTicksToWait )
+{
+    return ESP_OK;
+}
+
+uint32_t esp_log_timestamp()
+{
+    return 0;
+}
+
+esp_err_t httpd_resp_set_type(httpd_req_t *r, const char *type)
+{
+    return ESP_OK;
+}
+
